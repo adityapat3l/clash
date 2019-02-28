424c2c21e2beb85ccd63f147513b43a9e1ba9581
Aditya Patel <apatel@monetate.com>
2019-02-27 20:29:56 -0500

    created player and clan urls

---
+++ app/lib/urls.py
@@ -0,0 +1,19 @@
+
+BASE_URL = 'https://api.clashofclans.com/v1'
+
+# Clan Urls
+CLAN_SEARCH_URL = BASE_URL + '/clans'
+CLAN_URL = BASE_URL + '/clans/{path}'
+
+
+# Locations
+LOCATION_LIST_URL = BASE_URL + '/locations'
+LOCATION_URL = BASE_URL + '/locations/{path}'
+
+# Leagues
+LEAGUE_LIST_URL =  BASE_URL + '/leagues'
+LEAGUE_URL = BASE_URL + '/leagues/{path}'
+
+# Player URL
+PLAYER_URL = BASE_URL + '/players/{path}'
+
---
+++ app/main.py
@@ -0,0 +1,8 @@
+from app.collector import ClashAPI
+
+# print(ClashAPI.get_api_data('https://api.clashofclans.com/v1/players/%238292J8QV8'))
+
+
+# print(ClashAPI().get_player_info_from_tag('#8292J8QV8'))
+
+print(ClashAPI().get_clan_info_from_tag('#YUPCJJCR'))
