d947e22e9c7c04593be32c0d6343ac30348d5caf
Aditya Patel <apatel@monetate.com>
2019-03-03 16:02:12 -0500

    fixed instance collection

--- app/collector.py
+++ app/collector.py
@@ -1,9 +1,6 @@
 from __future__ import print_function
 from app.api import ClashAPI
 from pprint import pprint
-# pprint(ClashAPI().get_player_info_from_tag('#8292J8QV8'))
-
-# clan_data = pprint(ClashAPI().get_clan_info_from_tag('#YUPCJJCR'))
 
 
 class PlayerData:
@@ -27,32 +24,32 @@ class PlayerData:
     def parse_hero_info(self, heroList):
 
         for hero in heroList:
-            self.kingLevel = hero.get('level') if hero['name'] == 'Barbarian King' else None
-            self.battleMachineLevel = hero.get('level') if hero['name'] == 'Battle Machine' else None
-            self.queenLevel = hero.get('level') if hero['name'] == 'Archer Queen' else None
-            self.wardenLevel = hero.get('level') if hero['name'] == 'Grand Warden' else None
+            if hero['name'] == 'Barbarian King':
+                self.kingLevel = hero.get('level')
+            elif hero['name'] == 'Battle Machine':
+                self.battleMachineLevel = hero.get('level')
+            elif hero['name'] == 'Archer Queen':
+                self.queenLevel = hero.get('level')
+            elif hero['name'] == 'Grand Warden':
+                self.wardenLevel = hero.get('level')
 
     def get_player_info(self):
         player_info = ClashAPI().get_player_info_from_tag(self.tag)
         self.townHallLevel = player_info.get('townHallLevel')
         self.name = player_info.get('name')
-        # self.kingLevel = player_info.get('heroes').get('level')
+        self.donationsGiven = player_info.get('donations')
+        self.donationsReceived = player_info.get('donationsReceived')
+        self.warStars = player_info.get('warStars')
+        self.attackWins = player_info.get('attackWins')
+        self.defenseWins = player_info.get('defenseWins')
+
         self.parse_hero_info(player_info.get('heroes'))
 
-        # print(self.name, self.townHallLevel)
-
-        return player_info
-
-        # print(player_info.keys())
-
-
-
-
+        # print(player_info)
 
 
 class ClanData:
     def __init__(self, tag):
-        # super().__init__()
         self.tag = tag
         self.clanLevel = None
         self.clanPoints = None
@@ -60,25 +57,15 @@ class ClanData:
         self.warWins = None
         self.warLosses = None
         self.warTies = None
-        self.memberList = []
+        self.memberDict = {}
         self.numMembers = None
 
     def get_player_detail_list(self, player_list):
         self.numMembers = len(player_list)
 
-        player_dict = {}
         for member in player_list:
             player_tag = member.get('tag', None)
-            player_dict['tag'] = player_tag
-            # print(player_tag)
-
-            player_inst = PlayerData(player_tag)
-            player_dict['object'] = player_inst
-            self.memberList.append(player_inst.get_player_info())  # TODO: FIX THIS SHIT - SAVE INSTANCES NOT DICT
-
-            # print(self.memberList)
-            # print("\n\n\n")
-
+            self.memberDict[player_tag] = PlayerData(player_tag)
 
     def get_all_clan_info(self):
         allClanData = ClashAPI().get_clan_info_from_tag(self.tag)
@@ -93,18 +80,25 @@ class ClanData:
 
         self.get_player_detail_list(partialMemberList)
 
-        # print(self.memberList)
-
-    def get_townhall_counts(self):
+    def get_townhall_counts(self, active_only=False):
 
         townHallDict = {}
 
-        for player_dict in self.memberList:
-
-            if player_dict['townHallLevel'] in townHallDict.keys():
-                townHallDict[player_dict['townHallLevel']] += 1
+        def add_th():
+            if player.townHallLevel in townHallDict.keys():
+                townHallDict[player.townHallLevel] += 1
             else:
-                townHallDict[player_dict['townHallLevel']] = 1
+                townHallDict[player.townHallLevel] = 1
+
+        for player in self.memberDict.values():
+
+            player.get_player_info()
+
+            if active_only:
+                if player.donationsReceived + player.donationsGiven > 10:
+                    add_th()
+            else:
+                add_th()
 
         pprint(townHallDict)
 
--- app/main.py
+++ app/main.py
@@ -12,14 +12,20 @@ args = parser.parse_args()
 clan_tag = args.clan.lower() if args.clan else None
 player_tag = args.player if args.player else None
 
-if clan_tag == 'fy':
+if clan_tag == 'foraiur':
     clan_tag = '#YUPCJJCR'
-elif clan_tag == 'en':
+elif clan_tag == 'forever-young':
     clan_tag = '#CUQLRJOG'
-
-
+elif clan_tag == 'mincers':
+    clan_tag = '#9G2VRYUJ'
+elif clan_tag == 'elchavo':
+    clan_tag = '#2UPC98G'
+elif clan_tag == 'angrytigers':
+    clan_tag = '#YJQGP2VG'
+elif clan_tag == 'hamham':
+    clan_tag = '#GVJ9Q9J8'
 clan = ClanData(clan_tag)
 
 clan.get_all_clan_info()
-clan.get_townhall_counts()
-# pprint(PlayerData().get_player_info('#VVV8V9QC'))
+clan.get_townhall_counts(active_only=True)
+# pprint(PlayerData('#VVV8V9QC').get_player_info())
