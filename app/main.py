958ad53b11e27b53cd15e6fba70c51c63785b68f
Aditya Patel <apatel@monetate.com>
2019-03-03 16:47:01 -0500

    setting file structure

--- app/main.py
+++ app/main.py
@@ -5,12 +5,14 @@ import argparse
 
 
 parser = argparse.ArgumentParser(description="Enter Clan Info!")
-parser.add_argument("-c", "--clan", help="Your nobel profession")
-parser.add_argument("-p", "--player", help="Your nobel profession")
+parser.add_argument("-c", "--clan", help="Enter the clan tag with the #")
+parser.add_argument("-p", "--player", help="Enter the player tag with the $+#")
+parser.add_argument('-ao', "--active_only", help="Int - Only consider active players?", type=bool, default=False)
 args = parser.parse_args()
 
 clan_tag = args.clan.lower() if args.clan else None
 player_tag = args.player if args.player else None
+active_level = bool(int(args.active_only) if args.active_only else None)
 
 if clan_tag == 'foraiur':
     clan_tag = '#YUPCJJCR'
@@ -27,5 +29,6 @@ elif clan_tag == 'hamham':
 clan = ClanData(clan_tag)
 
 clan.get_all_clan_info()
-clan.get_townhall_counts(active_only=True)
+# clan.get_townhall_counts(active_only=True)
+clan.get_avg_hero_level_by_th(active_only=active_level)
 # pprint(PlayerData('#VVV8V9QC').get_player_info())
