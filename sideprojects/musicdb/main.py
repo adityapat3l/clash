8afe72c5b733c2f38c2961ff0759b931964476f2
Aditya Patel <apatel@monetate.com>
2019-03-19 16:33:39 -0400

    added music album

--- sideprojects/musicdb/main.py
+++ sideprojects/musicdb/main.py
@@ -3,6 +3,7 @@ from db_setup import init_db, db_session
 from forms import MusicSearchForm, AlbumForm
 from flask import flash, render_template, request, redirect
 from models import Album, Artist
+from tables import Results
 
 init_db()
 
@@ -21,7 +22,24 @@ def search_results(search):
     results = []
     search_string = search.data['search']
 
-    if search.data['search'] == '':
+    if search_string:
+        if search.data['select'] == 'Artist':
+            qry = db_session.query(Album, Artist).filter(
+                Artist.id==Album.artist_id).filter(
+                Artist.name.contains(search_string))
+            results = [item[0] for item in qry.all()]
+        elif search.data['select'] == 'Album':
+            qry = db_session.query(Album).filter(
+                Album.title.contains(search_string))
+            results = qry.all()
+        elif search.data['select'] == 'Publisher':
+            qry = db_session.query(Album).filter(
+                Album.publisher.contains(search_string))
+            results = qry.all()
+        else:
+            qry = db_session.query(Album)
+            results = qry.all()
+    else:
         qry = db_session.query(Album)
         results = qry.all()
 
@@ -30,7 +48,10 @@ def search_results(search):
         return redirect('/')
     else:
         # display results
-        return render_template('results.html', results=results)
+        table = Results(results)
+        table.border = True
+        return render_template('results.html', table=table)
+
 
 @app.route('/new_album', methods=['GET', 'POST'])
 def new_album():
@@ -69,6 +90,47 @@ def save_changes(album, form, new=False):
 
     # commit the data to the database
     db_session.commit()
+
+@app.route('/item/<int:id>', methods=['GET', 'POST'])
+def edit(id):
+    qry = db_session.query(Album).filter(
+        Album.id==id)
+    album = qry.first()
+
+    if album:
+        form = AlbumForm(formdata=request.form, obj=album)
+        if request.method == 'POST' and form.validate():
+            # save edits
+            save_changes(album, form)
+            flash('Album updated successfully!')
+            return redirect('/')
+        return render_template('edit_album.html', form=form)
+    else:
+        return 'Error loading #{id}'.format(id=id)
+
+
+@app.route('/delete/<int:id>', methods=['GET', 'POST'])
+def delete(id):
+    """
+    Delete the item in the database that matches the specified
+    id in the URL
+    """
+    qry = db_session.query(Album).filter(
+        Album.id==id)
+    album = qry.first()
+
+    if album:
+        form = AlbumForm(formdata=request.form, obj=album)
+        if request.method == 'POST' and form.validate():
+            # delete the item from the database
+            db_session.delete(album)
+            db_session.commit()
+
+            flash('Album deleted successfully!')
+            return redirect('/')
+        return render_template('delete_album.html', form=form)
+    else:
+        return 'Error deleting #{id}'.format(id=id)
 
 if __name__ == '__main__':
     app.run()
